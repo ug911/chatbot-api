e6297e758be713011ecb63090962ffab71df4c72
Utkarsh Gupta <utk.gupta@gmail.com>
2024-06-08 14:14:34 +0530

    Adding Generate Skills Options and Generate Skills Map API

--- myapp.py
+++ myapp.py
@@ -1,14 +1,85 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
-import boto3
-from urllib.parse import urlparse
-import requests
-import json
+from openai_assistant import ChatbotAssistant
 
 app = Flask(__name__)
 CORS(app)
 
+chatbot_assistant = ChatbotAssistant()
+
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/get_skills_options', methods=['POST'])
+def get_skills_options():
+    # Get the value of the 'additional_words' parameter from the POST request
+    name = request.form.get('name')
+    gender = request.form.get('gender')
+    designation = request.form.get('designation')
+    team = request.form.get('team')
+    industry = request.form.get('industry')
+    work_experience = request.form.get('work_experience')
+    day_at_work = request.form.get('day_at_work')
+    debug = request.form.get('debug')
+
+    prompt_variables = {
+        'designation': designation,
+        'team': team,
+        'industry': industry,
+        'work_experience': work_experience,
+        'day_at_work': day_at_work
+    }
+
+    msg = '\n'.join(['{}:{}'.format(k, v) for k, v in prompt_variables.items()])
+    print('{}/{}'.format(name, gender))
+    print(msg)
+    if not debug:
+        result = chatbot_assistant.generate_skills_options(prompt_variables=prompt_variables)
+    else:
+        result = {
+            'message': msg
+        }
+    return result
+
+
+@app.route('/get_skills_map', methods=['POST'])
+def get_skills_map():
+    # Get the value of the 'additional_words' parameter from the POST request
+    mongo_object_id = request.form.get('id')
+    name = request.form.get('name')
+    gender = request.form.get('gender')
+    designation = request.form.get('designation')
+    team = request.form.get('team')
+    industry = request.form.get('industry')
+    work_experience = request.form.get('work_experience')
+    day_at_work = request.form.get('day_at_work')
+    selected_skills = request.form.get('selected_skills')
+    additional_work = request.form.get('additional_work')
+    handover = request.form.get('handover')
+    debug = request.form.get('debug')
+
+    prompt_variables = {
+        'designation': designation,
+        'team': team,
+        'industry': industry,
+        'work_experience': work_experience,
+        'day_at_work': day_at_work,
+        'selected_skills': selected_skills,
+        'additional_work': additional_work,
+        'handover': handover
+    }
+
+    msg = '\n'.join(['{}:{}'.format(k, v) for k, v in prompt_variables.items()])
+    print('{}/{}'.format(name, gender))
+    print(msg)
+    if not debug:
+        result = chatbot_assistant.generate_skills_map(prompt_variables=prompt_variables, object_id=mongo_object_id)
+    else:
+        result = {
+            'message': msg
+        }
+    return result
 
--- openai_assistant.py
+++ openai_assistant.py
@@ -1,11 +1,10 @@
 import yaml
 import openai
-import gradio as gr
 from datetime import datetime
 from mongo_connect import MongoConnect
 
 
-class InterviewAssistant:
+class ChatbotAssistant:
     def __init__(self):
         # Initialize any necessary attributes or resources here
         with open("configs/config.yaml", "r") as f:
@@ -16,13 +15,9 @@ class InterviewAssistant:
             api_key=cx['openai']['api_key'],
         )
         self.mongo_client = MongoConnect()
-        self.object_id = self.mongo_client.create_new()
-        self.assistant_instructions = None
-        self.questions = None
-        self.skills = None
-        self.conversation_messages = []
-        self.analysis = None
-        self.assistant_id = self.get_assistant(name="Interview Assistant")
+        self.object_id = None
+        self.skills_options = None
+        self.skills_map = None
         self.model = cx['openai'].get('model', 'gpt-3.5-turbo')
         pass
 
@@ -56,113 +51,75 @@ class InterviewAssistant:
         })
         return response
 
-    def get_assistant(self, name="Interview Assistant"):
-        assistant_name = name
+    def generate_skills_options(self, prompt_variables):
+        """
+        Generates skills options for the employee based on input parameters
 
-        self.assistant_instructions = '''## Aim: 
-            - Ask questions to gather information about the skills of the user  
-            - Use the provided questions as a guideline to ask questions
+        Args:
+            prompt_variables: Variables to be used in prompts
 
-            ## Persona: 
-            - Always stay on point and do not get distracted
-            - Encourage user to think deeply and articulate their thoughts clearly
-            - Be encouraging and inquisitive
-            - Maintain professionalism in all circumstances
-            - Ensure the user feels supported throughout the conversation
-
-            ## Restrictions
-            - Do not deviate from a professional demeanor
-            - Do not use slang, jargon, or inappropriate language under any circumstances
-            - Do not provide answers or opinions on the topics discussed
-            - Do not disclose or discuss any specific reasons behind asking the questions
-
-            ## Ending the conversation
-            - End after all information is gathered
-            - End with this exact phrase "Thank you for your answers. Hope you have a good day. Bye.'''
-
-        all_assistants = self.client.beta.assistants.list(
-            order="desc",
-            limit="20",
+        Returns:
+            str: Generate Skills Options
+        """
+        self.object_id = self.mongo_client.create_new()
+        prompt = '''What should be skills for this person: 
+        Designation: {designation}
+        Team: {team}
+        Industry: {industry},
+        Work Experience: {work_experience},
+        Day at Work: {day_at_work}
+        Provide the categories and skills inside those categories
+        '''.format(
+            designation=prompt_variables['designation'],
+            team=prompt_variables['team'],
+            industry=prompt_variables['industry'],
+            work_experience=prompt_variables['work_experience'],
+            day_at_work=prompt_variables['day_at_work']
         )
 
-        assistant_id = None
-        for _asst in all_assistants.to_dict().get('data', []):
-            if _asst.get('name', '') == assistant_name:
-                assistant_id = _asst.get('id')
-                print('Found the assistant - {} | {}'.format(assistant_name, assistant_id))
-
-        if not assistant_id:
-            print("No Assistant Found.")
-            print("Creating a new assistant...")
-            assistant = self.client.beta.assistants.create(
-                name="Interview Assistant",
-                instructions=self.assistant_instructions,
-                model="gpt-3.5-turbo"
-            )
-            assistant_id = assistant.to_dict().get('id')
-        self.mongo_client.update({"$set": {"assistant_id": assistant_id}})
-        return assistant_id
-
-    def generate_questions(self, company_info, designation, department):
-        """
-        Generates interview questions based on input parameters.
-
-        Args:
-            company_info: Basic info about the company
-            designation: Employee designation in the company
-            department: Department where the employee works in the company
-
-        Returns:
-            str: Generated interview questions.
-        """
-        # Validate if all the parameters are available
-        if company_info == '' or designation == '' or department == '':
-            raise gr.Error("Can not generate questions without Company, Designation and Department")
-
-        # Create a prompt based on input parameters and use chat completion to generate questions
-        prompt = '''
-        You are an expert interviewer and have conducted various skill assessment interviews for tech people from all over the world.
-        You have help me interview a person who is {designation} in {department} working in a {company}. 
-        My main objective is to identfy the skill this person has. 
-        Give me a set of 10 questions I should ask this person such that I should be able to assess the skills this person has.
-        Note the Following:
-        1. I want you to generate questions which a friend would ask another friend. 
-        2. Do not be intrusive, the questions should not feel as if those questions are meant to judge them 
-        3. Ask encouraging questions which gets people excited to answer, giving them a chance to brag and highlight achievements
-        4. Be Highly informal in your tone but make sure you are always appropriate with no foul language
-        5. Ask general questions related to their designation to see their interest and expertise level.
-        6. Ask at least 2 questions about any programming languages, frameworks or tools used in everyday work and used in general.''' \
-            .format(designation=designation, department=department, company=company_info)
-
         messages = [{
             "role": "user",
             "content": prompt
         }]
 
-        # Use OpenAI chat completion to generate questions
-        self.questions = self.chat_completion(messages, key='questions')
-        return self.questions
+        # Use OpenAI chat completion to generate skills
+        self.skills_options = self.chat_completion(messages, key='skills')
+        return {
+            'id': self.object_id,
+            'skills_options': self.skills_options
+        }
 
-    def generate_skills(self, company_info, designation, department):
+    def generate_skills_map(self, prompt_variables, object_id):
         """
-        Generates skills for the employee based on input parameters.
+        Generates skills options for the employee based on input parameters
 
         Args:
-            company_info: Basic info about the company
-            designation: Employee designation in the company
-            department: Department where the employee works in the company
+            prompt_variables: Variables to be used in prompts
 
         Returns:
-            str: Generated interview questions.
+            str: Generate Skills Options
         """
-        # Validate if all the parameters are available
-        if company_info == '' or designation == '' or department == '':
-            raise gr.Error("Can not generate skills without Company, Designation and Department")
-
-        # Create a prompt based on input parameters and use chat completion to generate questions
-        prompt = ("What should be skills that a {designation} in {department} should have who is working in a " +
-                  "{company}? Provide the categories and skills inside those categories").format(
-            designation=designation, department=department, company=company_info)
+        self.object_id = object_id
+        prompt = '''What should be skills for this person: 
+        Designation: {designation}
+        Team: {team}
+        Industry: {industry},
+        Work Experience: {work_experience},
+        Day at Work: {day_at_work}
+        Selected Skills: {selected_skills}
+        Additional Tasks the person want to do for personal growth: {additional_work}
+        Tasks the person wants to handover: {handover}
+        Provide the categories and skills inside those categories
+        '''.format(
+            designation=prompt_variables['designation'],
+            team=prompt_variables['team'],
+            industry=prompt_variables['industry'],
+            work_experience=prompt_variables['work_experience'],
+            day_at_work=prompt_variables['day_at_work'],
+            selected_skills=prompt_variables['selected_skills'],
+            additional_work=prompt_variables['additional_work'],
+            handover=prompt_variables['handover']
+        )
 
         messages = [{
             "role": "user",
@@ -170,116 +127,8 @@ class InterviewAssistant:
         }]
 
         # Use OpenAI chat completion to generate skills
-        self.skills = self.chat_completion(messages, key='skills')
-        return self.skills
-
-    def respond_assistant(self, message, history):
-        """
-        Generates a response based on the given prompt.
-
-        Args:
-            message: Input message from the user
-            history: History of the conversation so far
-
-        Returns:
-            str: Generated response.
-        """
-        # Use chat completion to generate a response
-        # Implement your logic here
-        pass
-
-    def respond_completion(self, message, history):
-        """
-        Generates a response based on the given prompt.
-
-        Args:
-            message: Input message from the user
-            history: History of the conversation so far
-
-        Returns:
-            str: Generated response.
-        """
-        print(message)
-        completion_messages = self.build_completion_prompt(history)
-        completion_messages += [{"role": "user", "content": message}]
-
-        # Use OpenAI chat completion to generate a response
-        response = self.chat_completion(completion_messages, key='chat')
-        self.conversation_messages = completion_messages + [{"role": "assistant", "content": response}]
-        return response
-
-    def build_completion_prompt(self, history):
-        """
-        Using the history of the conversation, build the input message for completion
-
-        Args:
-            history: History of the conversation so far
-
-        Returns:
-            str: Analysis of the conversation.
-        """
-        # Create system instructions using assistant instructions and additional instructions
-        messages = [
-            {"role": "system", "content": self.assistant_instructions},
-            {"role": "system", "content": "## Questions to keep as guidelines:\n{questions}".format(
-                questions=self.questions
-            )},
-        ]
-
-        # Process the history and add User and Assistant message to the conversation
-        for _conv_step in history:
-            messages += [
-                {"role": "user", "content": _conv_step[0]},
-                {"role": "assistant", "content": _conv_step[1]},
-            ]
-        return messages
-
-    def build_conversation_text(self):
-        # messages = self.client.beta.threads.messages.list(self.thread_id, limit=100).to_dict()
-        # conversation_text = ''
-        # for _msg in messages['data'][::-1]:
-        #     conversation_text += '{}: {}\n'.format(_msg['role'], _msg['content'][0]['text']['value'])
-        conversation_text = ''
-        for _msg in self.conversation_messages:
-            conversation_text += '{}: {}\n'.format(_msg['role'], _msg['content'])
-        return conversation_text
-
-    def analyse_conversation(self):
-        """
-        Analyses a conversation based on skills and conversation input.
-
-        Returns:
-            str: Analysis of the conversation.
-        """
-        conversation_text = self.build_conversation_text()
-        analysis_prompt = '''## Aim: Based on the conversation, 
-        - Evaluate the user's skills
-        - For each of the mentioned skills, give a rating as mentioned
-        
-        ## Output
-        For each required skill, provide the following information
-        - Skill Present: Does conversation mention about the skill
-            - Value should be "Yes" if the skill is mentioned in the conversation
-            - Value should be "No" if the skill is not mentioned
-        - Level of knowledge of the skill: How well the user demonstrates this skill
-            - Good: Demonstrates exceptional abilities about the skill
-            - Medium: User demonstrates some abilities and willingness to learn
-            - Low: User lacks enough knowledge of the skill
-            - Not present: There is no mention of the skill in the conversation
-            
-        ## Skills
-        {skills}
-        
-        ## Conversation
-        {conversation}
-        '''.format(skills=self.skills, conversation=conversation_text)
-
-        messages = [{
-            "role": "user",
-            "content": analysis_prompt
-        }]
-
-        # Use OpenAI chat completion to generate skills
-        self.analysis = self.chat_completion(messages, key='analysis')
-        return self.analysis
-
+        self.skills_options = self.chat_completion(messages, key='skills')
+        return {
+            'id': self.object_id,
+            'skills_options': self.skills_options
+        }
